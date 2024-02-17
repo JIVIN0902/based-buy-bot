3e472ce758a410a63c3d6da2563de6b38762fc24
JIVIN0902 <jivinvaidya@gmail.com>
2024-02-17 13:37:33 +0530

    updates

--- track.js
+++ track.js
@@ -21,163 +21,6 @@ const {
 } = require("./utils");
 const { RPCS, topics, NATIVES } = require("./config");
 
-async function trackBuyAndSendMessage(
-  log,
-  buysCollection,
-  iface,
-  abi,
-  network,
-  provider
-) {
-  try {
-    const pool_address = log.address;
-    console.log(pool_address);
-
-    const chats = await buysCollection.find({
-      "pool.pairAddress": ethers.utils.getAddress(pool_address),
-    });
-    if (chats.length === 0) return;
-    const tx_hash = log.transactionHash;
-
-    const event = iface.parseLog(log);
-    const args = event.args;
-
-    const poolContract = new ethers.Contract(pool_address, abi, provider);
-    const token0 =
-      version === "v2" || version === "v3"
-        ? await poolContract.token0()
-        : await poolContract.tokenX();
-    const token1 =
-      version === "v2" || version === "v3"
-        ? await poolContract.token1()
-        : await poolContract.tokenY();
-    console.log(version, token0, token1, pool_address);
-    const token0Contract = new ethers.Contract(token0, ERC20_ABI, provider);
-    const token1Contract = new ethers.Contract(token1, ERC20_ABI, provider);
-
-    for (const chat of chats) {
-      console.log(chat.chat_id);
-      const pool = chat.pool;
-      const {
-        buy_step,
-        buy_emoji,
-        min_buy,
-        image,
-        chat_id,
-        tg_link,
-        twitter,
-        website,
-      } = chat;
-      const baseToken = pool.baseToken;
-      const quoteToken = pool.quoteToken;
-      const swap_data =
-        version === "v3"
-          ? get_data_v3(
-              args,
-              baseToken.address,
-              quoteToken.address,
-              token0,
-              token1
-            )
-          : version === "izi"
-          ? get_data_izi(
-              args,
-              baseToken.address,
-              quoteToken.address,
-              token0,
-              token1
-            )
-          : get_data_v2(
-              args,
-              baseToken.address,
-              quoteToken.address,
-              token0,
-              token1
-            );
-      console.log(swap_data);
-
-      let { amountIn, amountOut } = swap_data;
-      const to = version === "v3" ? args.recipient : args.to;
-      const token0Decimals = await token0Contract.decimals();
-      const token1Decimals = await token1Contract.decimals();
-      let userBalance = compareAddresses(token0, baseToken.address)
-        ? await token0Contract.balanceOf(to)
-        : await token1Contract.balanceOf(to);
-      userBalance = parseInt(userBalance.toString());
-      const totalSupply = compareAddresses(token0, baseToken.address)
-        ? await token0Contract.totalSupply()
-        : await token1Contract.totalSupply();
-      let tokenInDecimals = compareAddresses(token0, quoteToken.address)
-        ? token0Decimals
-        : token1Decimals;
-      tokenInDecimals = parseInt(tokenInDecimals.toString());
-      let tokenOutDecimals = compareAddresses(token0, baseToken.address)
-        ? token0Decimals
-        : token1Decimals;
-      tokenOutDecimals = parseInt(tokenOutDecimals.toString());
-
-      if (amountIn && amountOut) {
-        console.log(baseToken.symbol);
-        amountIn = parseInt(amountIn.toString());
-        amountOut = parseInt(amountOut.toString());
-        const position = getUserPosition(userBalance, amountOut);
-        amountIn = amountIn / 10 ** tokenInDecimals;
-        amountOut = amountOut / 10 ** tokenOutDecimals;
-        const prices = readPrices();
-        const quoteTokenPrice = prices[quoteToken.symbol];
-        const amountInUsd = amountIn * quoteTokenPrice;
-        const tokenPriceUsd = (amountIn / amountOut) * quoteTokenPrice;
-        const marketCap =
-          (tokenPriceUsd * totalSupply) / 10 ** tokenOutDecimals;
-        console.log(amountInUsd, tokenPriceUsd, marketCap);
-        const explorer = explorers[pool.chainId];
-        const native = NATIVES[network];
-        const nativePrice = prices[native];
-        const msg = `
-            <b>New ${baseToken.symbol} Buy!</b>\n
-            ${buy_emoji.repeat(process_number(amountInUsd, buy_step))}\n
-            💵 <b>Spent: </b> ${formatNumber(amountIn)} ${
-          quoteToken.symbol
-        } ($${formatNumber(amountInUsd)})
-            💰 <b>Bought: </b>${formatNumber(amountOut)} ${baseToken.symbol}
-            🏷️ <b>${baseToken.symbol} Price:</b> $${
-          tokenPriceUsd >= 0.000000001
-            ? formatNumber(tokenPriceUsd, 8)
-            : formatNumber(tokenPriceUsd, 14)
-        }
-            💲 ${native} Price: ${nativePrice}
-            🧔‍♂️ <b>Buyer: </b><a href="${explorer}/address/${to}">${to.slice(
-          0,
-          5
-        )}...${to.slice(38)}</a>
-            ${
-              position === Infinity
-                ? "✅ New Buyer"
-                : `⬆️ <b>Position:</b> ${(position < 0
-                    ? -1 * position
-                    : position
-                  ).toFixed(0)}%`
-            }
-            🏦 <b>Market Cap:</b> $${formatNumber(marketCap, 0)}\n
-            <a href='${explorer}/tx/${tx_hash}'>TX</a> | <a href='https://dexscreener.com/${
-          pool.chainId
-        }/${pool_address}'>CHART</a>${
-          tg_link ? ` | <a href='${tg_link}'>TG</a>` : ""
-        }${twitter ? ` | <a href='${twitter}'>TWITTER</a>` : ""}${
-          website ? ` | <a href='${website}'>WEBSITE</a>` : ""
-        }
-        `;
-        if (amountInUsd > min_buy) {
-          await sendTelegramMessage(dedent(msg), image, chat_id, network);
-          console.log("msg sent");
-        }
-      }
-    }
-  } catch (error) {
-    console.log(error);
-  }
-}
-
 async function trackBuys(network, version) {
   const provider = new ethers.providers.JsonRpcProvider(RPCS[network]);
   const db = new DB();
@@ -305,7 +148,7 @@ async function trackBuys(network, version) {
           const msg = `
             <b>New ${baseToken.symbol} Buy!</b>\n
             ${buy_emoji.repeat(process_number(amountInUsd, buy_step))}\n
-            💵 <b>Spent: </b> ${formatNumber(amountIn)} ${
+            💵 <b>Spent:</b> ${formatNumber(amountIn)} ${
             quoteToken.symbol
           } ($${formatNumber(amountInUsd)})
             💰 <b>Bought: </b>${formatNumber(amountOut)} ${baseToken.symbol}
@@ -314,11 +157,11 @@ async function trackBuys(network, version) {
               ? formatNumber(tokenPriceUsd, 8)
               : formatNumber(tokenPriceUsd, 14)
           }
-            💲 ${native} Price: ${nativePrice}
+            💲 <b>${native} Price:</b> $${nativePrice}
             🧔‍♂️ <b>Buyer: </b><a href="${explorer}/address/${to}">${to.slice(
             0,
             5
-          )}...${to.slice(38)}</a>
+          )}...${to.slice(38)}</a> | <a href='${explorer}/tx/${tx_hash}'>TX</a> 
             ${
               position === Infinity
                 ? "<b>✅ New Buyer</b>"
@@ -328,11 +171,11 @@ async function trackBuys(network, version) {
                   ).toFixed(0)}%`
             }
             🏦 <b>Market Cap:</b> $${formatNumber(marketCap, 0)}\n
-            <a href='${explorer}/tx/${tx_hash}'>TX</a> | <a href='https://dexscreener.com/${
-            pool.chainId
-          }/${pool_address}'>CHART</a>${
+            <a href='https://dexscreener.com/${
+              pool.chainId
+            }/${pool_address}'>📊 CHART</a>${
             tg_link ? ` | <a href='${tg_link}'>TG</a>` : ""
-          }${twitter ? ` | <a href='${twitter}'>TWITTER</a>` : ""}${
+          }${twitter ? ` | <a href='${twitter}'>X</a>` : ""}${
             website ? ` | <a href='${website}'>WEBSITE</a>` : ""
           }
         `;

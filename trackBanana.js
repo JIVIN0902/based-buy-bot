bd10b52ca0d37a8246fee0a9208968a28440d038
JIVIN0902 <jivinvaidya@gmail.com>
2024-04-17 09:46:52 +0530

    updates

--- trackBanana.js
+++ trackBanana.js
@@ -68,7 +68,7 @@ async function trackBuys(network, version) {
   provider.on(filter, async (log) => {
     try {
       const pool_address = log.address;
-      // console.log(network, pool_address);
+      console.log(network, pool_address);
 
       const chats = await buysCollection.find({
         "pool.pairAddress": ethers.utils.getAddress(pool_address),
@@ -261,13 +261,13 @@ async function trackBuys(network, version) {
 }
 
 let tasks = [];
-for (const network of BANANA_CHAINS) {
+for (const network of ["bitlayer"]) {
   for (const version of VERSIONS) {
     tasks.push(trackBuys(network, version));
   }
 }
 
-// scheduleJob("*/60 * * * * *", updatePrices);
+scheduleJob("*/60 * * * * *", updatePrices);
 // scheduleJob("*/60 * * * * *", updateTrending);
 // scheduleJob("0 * * * *", updateTrendingVolumes);
 
